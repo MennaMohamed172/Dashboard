b611d26e5d3be1cef18d9ed3c3b8742c0415feb9
menna <menna@mail.com>
2023-08-15 13:00:43 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let menuicn = document.querySelector(".menuicn");
+let nav = document.querySelector(".navcontainer");
+
+menuicn.addEventListener("click", () => {
+	nav.classList.toggle("navclose");
+})
+const input = document.querySelector("input")
+const output = document.querySelector("output")
+let imagesArray = []
+
+input.addEventListener("change", function() {
+})
+input.addEventListener("change", () => {
+    const file = input.files
+ })
+ 
+
+ const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark")
+})
